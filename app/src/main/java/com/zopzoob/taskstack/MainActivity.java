466dc05cbd765f1e8b30bbc29b1310ad6642dcf5
parmarad <parmar.ad@husky.neu.edu>
2017-02-14 00:40:58 -0500

    Phase 1B + 1C
    Basic structure for tag table created, also made simple UI

--- app/src/main/java/com/zopzoob/taskstack/MainActivity.java
+++ app/src/main/java/com/zopzoob/taskstack/MainActivity.java
@@ -30,15 +30,29 @@ public class MainActivity extends AppCompatActivity {
         tags            1
         duration        2 (with type int, representing multiple of thirty minutes)
         deadline        2
-        reward          3
-
+        status          3 (to do real agile-y stuff by "working" on several tasks for a period of time
+        reward          4
 
          */
         mastermind.execSQL("CREATE TABLE IF NOT EXISTS TasksV1 (title VARCHAR, description VARCHAR, id INTEGER PRIMARY KEY)");
 
+
+        /*
+        creates a table representing a tag (all tag tables will have a same format but will have a different name)
+
+        CURRENT STRUCTURE:
+        taskID          INT, the id of the task
+
+        THINGS I WANT:
+        some way to uniquely identify what the tag should be called based on the table name
+        example: table is called due_in_one_day --> tag is called "due in one day"
+
+        wait maybe I should just remove underscores and put in spaces...
+         */
+        mastermind.execSQL("CREATE TABLE IF NOT EXISTS due_today (taskID INT)");
+        mastermind.execSQL("CREATE TABLE IF NOT EXISTS school (taskID INT)");
+        mastermind.execSQL("CREATE TABLE IF NOT EXISTS misc (taskID INT)");
+
     }
 
-    public void tapped(View view) {
-        Log.i("button", "success");
-    }
 }
