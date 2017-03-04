7f1a8215d10b873c304d610060cc061c150f74ee
parmarad <parmar.ad@husky.neu.edu>
2017-03-04 11:55:34 -0500

    couple testing thingies
    test test

--- app/src/main/java/com/zopzoob/taskstack/CreateTask.java
+++ app/src/main/java/com/zopzoob/taskstack/CreateTask.java
@@ -18,6 +18,59 @@ import android.widget.Toast;
 
 public class CreateTask extends AppCompatActivity {
 
+    private SQLiteDatabase mastermind;
+
+    protected void getHi(View view) {
+        try {
+            Cursor c = mastermind.rawQuery("SELECT * FROM TasksV1 ORDER BY id DESC LIMIT 1", null);
+
+            c.moveToFirst();
+            while(c != null) {
+                Log.i("dbdbd - name", c.getString(c.getColumnIndex("title")));
+                Log.i("dbdbd - description", c.getString(c.getColumnIndex("description")));
+                Log.i("dbdbd - id", c.getString(c.getColumnIndex("id")));
+                c.moveToNext();
+            }
+            c.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void clearStack(View view) {
+        EditText tDesc = (EditText) findViewById(R.id.enterDesc);
+
+        try {
+           int i = Integer.parseInt(tDesc.getText().toString());
+            mastermind.execSQL("DELETE FROM TasksV1 WHERE id = " + i);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void makeLogs(View view) {
+
+        try {
+            //    Cursor c = mastermind.rawQuery("SELECT * FROM TasksV1 WHERE title = " + name, null);
+            Cursor c = mastermind.rawQuery("SELECT * FROM TasksV1", null);
+
+            c.moveToFirst();
+            while(c != null) {
+                Log.i("dbdbd - name", c.getString(c.getColumnIndex("title")));
+                Log.i("dbdbd - description", c.getString(c.getColumnIndex("description")));
+                Log.i("dbdbd - id", c.getString(c.getColumnIndex("id")));
+                c.moveToNext();
+            }
+            c.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
     protected void makeTask(View view) {
         EditText tName = (EditText) findViewById(R.id.enterName);
         EditText tDesc = (EditText) findViewById(R.id.enterDesc);
@@ -31,20 +84,9 @@ public class CreateTask extends AppCompatActivity {
         }
 
         try {
-            SQLiteDatabase mastermind = this.openOrCreateDatabase("Mastermind", MODE_PRIVATE, null);
             mastermind.execSQL("INSERT INTO TasksV1 (title,description) VALUES ('" + name + "','" + desc + "')");
 
-        //    Cursor c = mastermind.rawQuery("SELECT * FROM TasksV1 WHERE title = " + name, null);
-            Cursor c = mastermind.rawQuery("SELECT * FROM TasksV1", null);
 
-            c.moveToFirst();
-            while(c != null) {
-                Log.i("name", c.getString(c.getColumnIndex("title")));
-                Log.i("description", c.getString(c.getColumnIndex("description")));
-                Log.i("id", c.getString(c.getColumnIndex("id")));
-                c.moveToNext();
-            }
-            c.close();
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -54,11 +96,13 @@ public class CreateTask extends AppCompatActivity {
 
         return;
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
 
+        mastermind = openOrCreateDatabase("Mastermind", MODE_PRIVATE, null);
         Intent intent = getIntent();
 
 
--- app/src/main/java/com/zopzoob/taskstack/StackPanel.java
+++ app/src/main/java/com/zopzoob/taskstack/StackPanel.java
@@ -13,7 +13,7 @@ public class StackPanel extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_task);
+        setContentView(R.layout.activity_stack);
 
         Intent intent = getIntent();
 
